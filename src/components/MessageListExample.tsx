59f7d0e6f9ea65333af69cf68ced74ca05ab7cb3
Tiffany Wang <thiffanyjun@hotmail.com>
2024-07-30 21:48:23 +1200

    live chat UI

--- src/components/MessageListExample.tsx
+++ src/components/MessageListExample.tsx
@@ -1,20 +1,21 @@
 import { useRef, useState } from 'react'
-import { Button } from 'react-chat-elements'
 import { Input } from 'react-chat-elements'
 import { MessageList } from 'react-chat-elements'
-import { token } from '../utils/common'
 import {
-    audioMessage,
     fileMessage,
-    locationMessage,
-    meetingLinkMessage,
-    meetingMessage,
     photoMessage,
-    spotifyMessage,
     systemMessage,
     textMessage,
-    videoMessage,
+    sentTextMessage,
 } from '../utils/MessageTypes'
+import { RiAttachment2 } from 'react-icons/ri'
+import { FaArrowCircleUp } from 'react-icons/fa'
+import { BsChatTextFill } from 'react-icons/bs'
+import { AiFillCloseCircle } from 'react-icons/ai'
+import { FaRegFileAlt } from 'react-icons/fa'
+import { FaRegImage } from 'react-icons/fa6'
+import '../App.css'
+import { createSentTextMessage } from '../utils/common'
 
 let clearRef = () => {}
 
@@ -24,136 +25,93 @@ function useForceUpdate() {
 }
 
 function MessageListExample() {
-    const [messageListArray, setMessageListArray] = useState<any>([])
-    const [status, setStatus] = useState<string>('')
+    const sentTextMessage1 = createSentTextMessage('Sure!')
+    const sentTextMessage2 = createSentTextMessage('Received, thanks!')
+    const chatDemoMessages = [
+        textMessage,
+        sentTextMessage1,
+        fileMessage,
+        photoMessage,
+        sentTextMessage2,
+        // systemMessage,
+    ]
+    const [messageListArray, setMessageListArray] =
+        useState<any>(chatDemoMessages)
+    const [chatOpen, setChatOpen] = useState<boolean>(false)
+    const [uploadOpen, setUploadOpen] = useState<boolean>(false)
     const messageListReferance = useRef()
     const inputReferance = useRef()
 
-    const forceUpdate = useForceUpdate()
-
-    const addMessage = (data: number) => {
-        let Addmtype: string | number = data || token()
-        switch (data) {
-            case 0:
-                Addmtype = 'photo'
-                setStatus('waiting')
-                break
-            case 1:
-                Addmtype = 'file'
-                setStatus('sent')
-                break
-            case 2:
-                Addmtype = 'system'
-                break
-            case 3:
-                Addmtype = 'location'
-                setStatus('received')
-                break
-            case 4:
-                Addmtype = 'spotify'
-                setStatus('waiting')
-                break
-            case 5:
-                Addmtype = 'meeting'
-                setStatus('sent')
-                break
-            case 6:
-                Addmtype = 'video'
-                setStatus('read')
-                break
-            case 7:
-                Addmtype = 'audio'
-                break
-            case 8:
-                Addmtype = 'meetingLink'
-                break
-            default:
-                Addmtype = 'text'
-                setStatus('read')
-                break
-        }
-
-        setMessageListArray([...messageListArray, randomMessage(Addmtype)])
-        clearRef()
-        forceUpdate()
-    }
-
-    const randomMessage = (type: string) => {
-        switch (type) {
-            case 'photo':
-                return photoMessage
-            case 'file':
-                return fileMessage
-            case 'system':
-                return systemMessage
-            case 'location':
-                return locationMessage
-            case 'spotify':
-                return spotifyMessage
-            case 'meeting':
-                return meetingMessage
-            case 'video':
-                return videoMessage
-            case 'audio':
-                return audioMessage
-            case 'meetingLink':
-                return meetingLinkMessage
-            case 'text':
-                return textMessage
-            default:
-                break
-        }
-    }
-
     return (
-        <div className="right-panel rce-example-messageList">
-            <MessageList
-                className="message-list"
-                referance={messageListReferance}
-                dataSource={messageListArray}
-                lockable={true}
-                downButton={true}
-                downButtonBadge={10}
-                sendMessagePreview={true}
-            />
-
-            <div
-                style={{
-                    position: 'fixed',
-                    bottom: 0,
-                    right: 0,
-                    left: 0,
-                    margin: '0 auto 1rem auto',
-                    width: '60%',
-                }}
-            >
-                <Input
-                    className="rce-example-input"
-                    placeholder="Write your message here."
-                    defaultValue=""
-                    multiline={true}
-                    maxlength={300}
-                    onMaxLengthExceed={() => console.log('onMaxLengthExceed')}
-                    referance={inputReferance}
-                    clear={(clear: any) => (clearRef = clear)}
-                    maxHeight={50}
-                    onKeyPress={(e: any) => {
-                        if (e.shiftKey && e.charCode === 13) {
-                            return true
-                        }
-                        if (e.charCode === 13) {
-                            clearRef()
-                            addMessage(token())
-                        }
-                    }}
-                    rightButtons={
-                        <Button
-                            text="Submit"
-                            onClick={() => addMessage(token())}
-                        />
-                    }
-                />
+        <div className="chat-window-container">
+            <div className="chat-icon-container">
+                {chatOpen ? (
+                    <AiFillCloseCircle
+                        size={50}
+                        onClick={() => setChatOpen(false)}
+                    />
+                ) : (
+                    <BsChatTextFill
+                        size={50}
+                        onClick={() => setChatOpen(true)}
+                    />
+                )}
             </div>
+            {chatOpen && (
+                <div className="right-panel rce-example-messageList">
+                    <div className="rce-example-heading">
+                        <h2>Chat</h2>
+                    </div>
+                    <MessageList
+                        className="message-list"
+                        referance={messageListReferance}
+                        dataSource={messageListArray}
+                        lockable={true}
+                        downButton={true}
+                        downButtonBadge={10}
+                        sendMessagePreview={true}
+                    />
+
+                    <div className="rce-example-input-container">
+                        <div
+                            className="attachment-icon-container"
+                            onClick={() => setUploadOpen(!uploadOpen)}
+                        >
+                            <RiAttachment2 size={30} />
+                        </div>
+
+                        <Input
+                            className="rce-example-input"
+                            placeholder="Write your message here."
+                            defaultValue=""
+                            multiline={true}
+                            maxlength={300}
+                            onMaxLengthExceed={() =>
+                                console.log('onMaxLengthExceed')
+                            }
+                            referance={inputReferance}
+                            clear={(clear: any) => (clearRef = clear)}
+                            maxHeight={50}
+                            onKeyPress={(e: any) => {
+                                if (e.shiftKey && e.charCode === 13) {
+                                    return true
+                                }
+                                if (e.charCode === 13) {
+                                    clearRef()
+                                    // addMessage()
+                                }
+                            }}
+                            rightButtons={<FaArrowCircleUp size={30} />}
+                        />
+                    </div>
+                    {uploadOpen && (
+                        <div className="file-upload-icons-container">
+                            <FaRegFileAlt size={25} />
+                            <FaRegImage size={25} />
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     )
 }
